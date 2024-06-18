373e00e98b9fe21ca28f0eb2b664b725aed9390c
NivethaRS <NivethaRS@192.168.0.12>
2024-06-18 11:13:22 -0400

    strings

--- src/main/java/firstmaven/strings.java
+++ src/main/java/firstmaven/strings.java
@@ -8,11 +8,11 @@ public class strings {
 
 	public static void main(String[] args) {
 		// string literal- does not allow duplicate strings
-        /*String s ="This is automation";
+        String s ="This is automation";
         String s1 ="This is automation";
         System.out.println(s + s1);
-	}*/
-	// string -using new memory allocator
+	}
+	// string -using new memory allocator KW
     String s = new String("Hi");
     String s1 =new String("Java");
     System.out.println(s +" "+ s1);
