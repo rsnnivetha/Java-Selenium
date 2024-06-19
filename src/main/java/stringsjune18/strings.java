4fa70617f1c811b6cffea132e0b7f42cfc4debc8
NivethaRS <NivethaRS@PQA-0778LT>
2024-06-19 09:28:40 -0400

    demo

--- src/main/java/stringsjune18/strings.java
+++ src/main/java/stringsjune18/strings.java
@@ -1,4 +1,4 @@
-package firstmaven;
+package stringsjune18;
 
 public class strings {
 
@@ -8,14 +8,47 @@ public class strings {
 
 	public static void main(String[] args) {
 		// string literal- does not allow duplicate strings
-        String s ="This is automation";
+       /* String s ="This is automation";
         String s1 ="This is automation";
-        System.out.println(s + s1);
-	}
+        System.out.println(s == s1);*/
+	
 	// string -using new memory allocator KW
-    String s = new String("Hi");
-    String s1 =new String("Java");
-    System.out.println(s +" "+ s1);
+	/*
+	 * String a=new String g("Hi"); String s1 =new String("Java");
+	 * System.out.println(s+s1);
+	 */ 
+	
+	  //returns char value for an index
+	  String name = "I am learning";
+	  System.out.println(name.charAt(2));
+	  
+	  //length of string
+	  System.out.println(name.length());
+	  
+	  //checks if the string is equal
+	  System.out.println(name.equals("Nandeesvar"));
+	  
+	  //checks if the string is equal ignoring the case-sensitive
+	  System.out.println(name.equalsIgnoreCase("NIVETHA"));
+	  
+	  //check if the string is empty
+	  System.out.println(name.isEmpty());
+	  
+	  //take a particular portion of the string or trimming concept
+	  System.out.println(name.substring(2));
+	  System.out.println(name.substring(1,4));
+	  
+	  //concatenating the string
+	  System.out.println(name.concat(" Automation"));
+	  
+	  //replace the existing char with the given char
+	  System.out.println(name.replace("I am","Nivetha is "));
+	  
+	 
+	  
+	  
+	  
+}
+}
 
-}
-}
+
